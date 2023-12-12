fdbb6d38336bd433aecee2f7fbcd2f43b4c45739
Ander Cabrera <andercabrerac@gmail.com>
2023-12-12 00:42:20 -0600

    basic demostration done

--- src/sketch.js
+++ src/sketch.js
@@ -1,3 +1,17 @@
+function removeFromArray(array, element) {
+  for (let i = array.length; i >= 0; i--) {
+    if (array[i] === element) {
+      array.splice(i, 1);
+    }
+  }
+}
+
+function heuristics(a, b) {
+  // distance using euclidian distance (pitagoras theorem)
+  let d = dist(a.i, a.j, b.i, b.j);
+  return d;
+}
+
 let cols = 5;
 let rows = 5;
 let grid = new Array(cols);
@@ -14,17 +28,41 @@ let w, h;
 
 class Spot {
   constructor(i, j) {
-    this.x = i;
-    this.y = j;
+    // x
+    this.i = i;
+    // y
+    this.j = j;
     this.f = 0;
     this.g = 0;
     this.h = 0;
+    this.neightbors = [];
   }
 
   show(color) {
     fill(color);
     noStroke();
-    rect(this.x * w, this.y * h, w - 1, h - 1);
+    rect(this.i * w, this.j * h, w - 1, h - 1);
+  }
+
+  addNeightbors(grid) {
+    let i = this.i;
+    let j = this.j;
+
+    if (i < cols - 1) {
+      this.neightbors.push(grid[i + 1][j])
+    }
+
+    if (i > 0) {
+      this.neightbors.push(grid[i - 1][j])
+    }
+
+    if (j < rows - 1) {
+      this.neightbors.push(grid[i][j + 1])
+    }
+
+    if (j > 0) {
+      this.neightbors.push(grid[i][j - 1])
+    }
   }
 }
 
@@ -46,17 +84,61 @@ function setup() {
     }
   }
 
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j].addNeightbors(grid);
+    }
+  }
+
   start = grid[0][0];
   end = grid[cols - 1][rows - 1];
 
   openSet.push(start);
-
   console.log(grid);
 }
 
 function draw() {
-  if (openSet > 0) {
-    // starting algorithm
+  if (openSet.length > 0) {
+    console.log('xd');
+    let winner = 0;
+
+    for (let i = 0; i < openSet.length; i++) {
+      if (openSet[i].f < openSet[winner].f) {
+        winner = i;
+      }
+    }
+
+    let current = openSet[winner];
+
+    if (current === end) {
+      console.log('Done!');
+    }
+
+    removeFromArray(openSet, current);
+    closedSet.push(current);
+
+    let neightbors = current.neightbors;
+
+    for (let i = 0; i < neightbors.length; i++) {
+      let neightbor = neightbors[i];
+
+      if (!closedSet.includes(neightbor)) {
+        let tempG = current.g + 1;
+
+        if (openSet.includes(neightbor)) {
+          if (tempG < neightbor.g) {
+            neightbor.g = tempG;
+          }
+        } else {
+          neightbor.g = tempG;
+          openSet.push(neightbor);
+        }
+
+        neightbor.h = heuristics(neightbor, end);
+        neightbor.f = neightbor.g + neightbor.h;
+      }
+    }
+
   } else {
     // no solution
   }
@@ -68,12 +150,13 @@ function draw() {
       grid[i][j].show(color(255));
     }
   }
-
-  for (let i = 0; i < closedSet.length; i++) {
-    closedSet[i].show(color(255, 0, 0));
-  }
   
   for (let i = 0; i < openSet.length; i++) {
     openSet[i].show(color(0, 255, 0));
+  }
+
+  
+  for (let i = 0; i < closedSet.length; i++) {
+    closedSet[i].show(color(255, 0, 0));
   }
 }
