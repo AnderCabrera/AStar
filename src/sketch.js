7fd816180d7ea3d411503aedce89aa31198728e5
Ander Cabrera <andercabrerac@gmail.com>
2024-02-21 00:07:06 -0600

    adding final path and starting with walls

--- src/sketch.js
+++ src/sketch.js
@@ -8,23 +8,25 @@ function removeFromArray(array, element) {
 
 function heuristics(a, b) {
   // distance using euclidian distance (pitagoras theorem)
-  let d = dist(a.i, a.j, b.i, b.j);
+  // let d = dist(a.i, a.j, b.i, b.j);
+
+  // Manhattan
+  let d = abs(a.i - b.i) + abs(a.j - b.j);
   return d;
 }
 
-let cols = 5;
-let rows = 5;
+let cols = 20;
+let rows = 20;
 let grid = new Array(cols);
 
 // the spots to be reviewed
 let openSet = [];
 // the spots reviewed
 let closedSet = [];
-
 let start;
 let end;
-
 let w, h;
+let path = [];
 
 class Spot {
   constructor(i, j) {
@@ -36,10 +38,19 @@ class Spot {
     this.g = 0;
     this.h = 0;
     this.neightbors = [];
+    this.previous = undefined;
+    this.wall = false;
+
+    if (random(1) < 0.1) {
+      this.wall = true;
+    }
   }
 
   show(color) {
     fill(color);
+    if (this.wall) {
+      fill(0);
+    }
     noStroke();
     rect(this.i * w, this.j * h, w - 1, h - 1);
   }
@@ -49,26 +60,26 @@ class Spot {
     let j = this.j;
 
     if (i < cols - 1) {
-      this.neightbors.push(grid[i + 1][j])
+      this.neightbors.push(grid[i + 1][j]);
     }
 
     if (i > 0) {
-      this.neightbors.push(grid[i - 1][j])
+      this.neightbors.push(grid[i - 1][j]);
     }
 
     if (j < rows - 1) {
-      this.neightbors.push(grid[i][j + 1])
+      this.neightbors.push(grid[i][j + 1]);
     }
 
     if (j > 0) {
-      this.neightbors.push(grid[i][j - 1])
+      this.neightbors.push(grid[i][j - 1]);
     }
   }
 }
 
 function setup() {
   createCanvas(400, 400);
-  console.log("A*");
+  console.log('A*');
 
   w = width / cols;
   h = height / rows;
@@ -110,7 +121,19 @@ function draw() {
 
     let current = openSet[winner];
 
+    path = [];
+    let temp = current;
+    path.push(temp);
+
+    while (temp.previous) {
+      path.push(temp.previous);
+      temp = temp.previous;
+    }
+
     if (current === end) {
+      // Find the path
+      noLoop();
+
       console.log('Done!');
     }
 
@@ -136,9 +159,9 @@ function draw() {
 
         neightbor.h = heuristics(neightbor, end);
         neightbor.f = neightbor.g + neightbor.h;
+        neightbor.previous = current;
       }
     }
-
   } else {
     // no solution
   }
@@ -150,13 +173,16 @@ function draw() {
       grid[i][j].show(color(255));
     }
   }
-  
+
   for (let i = 0; i < openSet.length; i++) {
     openSet[i].show(color(0, 255, 0));
   }
 
-  
   for (let i = 0; i < closedSet.length; i++) {
     closedSet[i].show(color(255, 0, 0));
+  }
+
+  for (let i = 0; i < path.length; i++) {
+    path[i].show(color(0, 0, 255));
   }
 }
